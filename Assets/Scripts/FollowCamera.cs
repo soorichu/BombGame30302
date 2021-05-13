2d848ceca5369357da5005ae630051097ff45217
Soori <soorichu@gmail.com>
2021-05-13 10:27:53 +0900

    Initial commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target; 
+    public float dist = 7.0f; 
+    public float height = 4.0f;
+    void LateUpdate() { 
+        transform.position = target.position 
+                        - (target.forward * dist) 
+                        + (Vector3.up * height); 
+        transform.LookAt(target); 
+    }
+}
